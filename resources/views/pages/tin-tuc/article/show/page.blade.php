fffa91a295c1fa0a7b438263bfa6e844ab02ae89
chien.le <chien.hcckt@gmail.com>
2023-11-02 00:50:18 +0700

    add button and fix header

--- resources/views/pages/tin-tuc/article/show/page.blade.php
+++ resources/views/pages/tin-tuc/article/show/page.blade.php
@@ -16,30 +16,54 @@
         {!! $article->content!!}
     </div>
     <div class="d-flex align-content-center justify-content-start">
-        <a href="{{route('tin-tuc.index')}}" class="btn green-bg btn-block mb-3 rounded-pill border-2 border-white text-white mitr-medium super-long shadow-test button-shake">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
-              <style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
-            Xem nhiều bài hơn...</a>
+
+        <a href="{{route('tin-tuc.index')}}"
+           class="btn  btn-block mb-3 rounded-pill border-1 monte-semibold super-long button-shake">
+           <span>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1.1em" viewBox="0 0 448 512">
+                <style>svg {
+                        fill: lightslategrey
+                    }</style><path
+                    d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+            </span>
+            <span>XEM THÊM BÀI VIẾT</span>
+        </a>
     </div>
 </div>
 <style>
-    body{
+    body {
     }
-    .super-long{
-      width: 250px
+
+    .monte-semibold {
+        font-family: Montserrat-Bold, sans-serif;
+        font-size: 15px;
+        text-align: center;
+        color: lightslategrey;
+        border-color: lightslategrey;
     }
-    .datetime_text{
+    .monte-semibold:hover{
+        color: lightslategrey !important;
+    }
+
+    .super-long {
+        width: 215px
+    }
+
+    .datetime_text {
         font-family: Mulish-SemiBold, sans-serif;
         color: #F79421;
     }
+
     .content-article {
-      margin: 1rem 20rem 5rem 20rem;
+        margin: 1rem 20rem 5rem 20rem;
         background-color: white;
     }
+
     .tilte-text {
         color: #59843d;
         font-family: "Mulish Black", sans-serif;
     }
+
     .figure-image {
         display: flex;
         justify-content: center;
@@ -48,9 +72,10 @@
 
     @media only screen and (max-width: 1300px) {
         .content-article {
-            margin: 3rem 10rem 10rem 10rem ;
+            margin: 3rem 10rem 10rem 10rem;
         }
     }
+
     @media only screen and (max-width: 1000px) {
         .content-article {
             margin: 3rem 5rem 10rem 5rem;
@@ -61,6 +86,7 @@
         .content-article {
             margin: 3rem 1rem 3rem 1rem;
         }
+
         .tilte-text {
             color: #59843d;
             font-family: "Mulish Black", sans-serif;
@@ -69,7 +95,7 @@
     }
 </style>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('.image img').addClass('img-fluid');
         $('.image ').addClass('figure-image');
     });
